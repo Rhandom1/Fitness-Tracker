4edbf599727c983d451489edad242a4f40ae5ece
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-10-28 00:04:21 -0500

    Added workout model

--- models/workout.js
+++ models/workout.js
@@ -9,8 +9,55 @@ const workoutSchema = new Schema({
     excercise: [
         {
             type: {
-                
-            }
-        }
-    ]
-})
+                type: String,
+                trim: true,
+                required: "Enter the type of excercise"
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter a name for the excercise"
+            },
+
+            duration: {
+                type: Number,
+                trim: true,
+                required: "Enter the duration of the excercise"
+            },
+            weight: {
+                type: Number,
+                trim: true,
+                reuired: true
+            },
+
+            reps: {
+                type: Number,
+                trim: true,
+                reuired: true
+            },
+            
+            sets: {
+                type: Number,
+                trim: true,
+                reuired: true
+            },
+
+            distance: {
+                type: Number,
+                trim: true,
+                reuired: true
+            },
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    },
+);
+
+const Workout = mongoose.model("Worout", workoutSchema);
+
+module.exports = Workout;
