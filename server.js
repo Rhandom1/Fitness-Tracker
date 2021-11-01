be29b3ef4e6a2a85c5ecfc21aca19157abe290c1
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-11-01 18:11:13 -0500

    added require for env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+require ("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
