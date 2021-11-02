ba14cb1a7a0772d4cf197ccc46509beadbad6682
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-11-01 21:31:54 -0500

    updated MONGO connect, now works on Atlas

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(apiRoutes);
 const homeRoutes = require("./controllers/homeRoutes");
 app.use(homeRoutes);
 
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true
